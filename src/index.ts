b3b5eeb3861cf4db634e98fa3c03a0e6451f70f5
parkjihwanjay <cc6656@maver.com>
2021-06-20 14:41:19 +0900

    feat: index 수정

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import 'reflect-metadata';
+import * as express from 'express';
+
 import { createConnection } from 'typeorm';
 import { User } from './entity/User';
 
-createConnection()
-  .then(async (connection) => {
-    console.log('connection succed');
-  })
-  .catch((error) => console.log(error));
+const app = express();
+const port = 3000;
+
+const init = async () => {
+  const connection = await createConnection();
+
+  app.get('/', (req, res) => {
+    const user = new User();
+    res.json(user);
+  });
+
+  app.listen(port, () => {
+    console.log('express server running');
+  });
+};
+
+init();
